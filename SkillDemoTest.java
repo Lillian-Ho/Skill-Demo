3db9f0f7015e3a952fab4f97a073e307613d52fc
LillianHo5 <lmho@ucsd.edu>
2022-02-03 23:52:15 -0800

    Update SkillDemoTest.java
    Fixing failing test

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() { 
-        assertEquals(25, SkillDemo.multiply(3, 1)); //this test would fail
+        assertEquals(3, SkillDemo.multiply(3, 1)); //this test would pass
     }
 }
